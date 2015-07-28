2c601f8c193153222e320ea58a12ef8c0c3c2dc5
Planeshifter <pburckhardt@outlook.com>
2015-07-29 00:28:31 +0200

    [INIT]

---
+++ test/fixtures/test.accessor.R
@@ -0,0 +1,19 @@
+options( digits = 16 )
+library( jsonlite )
+
+
+alpha = 1
+beta = 1
+probs = seq( 0, 1, 0.01 )
+y = qbeta( probs, alpha, beta )
+
+cat( y, sep = ",\n" )
+
+data = list(
+	alpha = alpha,
+	beta = beta,
+	data = probs,
+	expected = y
+)
+
+write( toJSON( data, digits = 16, auto_unbox = TRUE ), "./test/fixtures/accessor.json" )
---
+++ test/fixtures/test.number.R
@@ -0,0 +1,19 @@
+options( digits = 16 )
+library( jsonlite )
+
+
+alpha = 1
+beta = 1
+probs = c( 0, 0.25, 0.5, 0.75, 1 )
+y = qbeta( probs, alpha, beta )
+
+cat( y, sep = ",\n" )
+
+data = list(
+	alpha = alpha,
+	beta = beta,
+	data = probs,
+	expected = y
+)
+
+write( toJSON( data, digits = 16, auto_unbox = TRUE ), "./test/fixtures/number.json" )
